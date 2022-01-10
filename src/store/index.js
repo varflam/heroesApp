4b5044fe9c78b1ce7dda1557e92caf2121966df6
Maria <alece.1414@gmail.com>
2022-01-10 22:29:06 +0300

    middleware&reacttoolkit added

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,23 @@
-import { createStore, combineReducers } from 'redux';
+import {configureStore} from '@reduxjs/toolkit';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
 
-const store = createStore( combineReducers({heroes, filters}), 
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const stringMiddleware = () => (next) => (action) => {
+    if(typeof action === 'string') {
+        return next({
+            type: action
+        })
+    } 
+    return next(action)
+}
+
+// const store = createStore( combineReducers({heroes, filters}), 
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+const store = configureStore({
+    reducer: {heroes, filters},
+    middlewares: getDefaultMiddleware => getDefaultMiddleware(stringMiddleware),
+    devTools: process.env.NODE_ENV !== 'production'
+});
 
 export default store;
